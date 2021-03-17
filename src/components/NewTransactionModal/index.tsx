4f862cb0659dfa2d7c44cd67d53a95bfdb8b276a
João Marcos Magalhães Madureira <joaommmadureira@hotmail.com>
2021-03-17 12:20:35 -0300

    finishing styling the new transaction modal

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,4 +1,9 @@
+import { useState } from 'react';
 import Modal from 'react-modal';
+import closeImg from '../../assets/close.svg'
+import incomeImg from '../../assets/income.svg'
+import outcomeImg from '../../assets/outcome.svg'
+import { Container, TransctionTypeContainer, TypeBox } from './styles';
 
 interface NewTransactionModalProps {
     isOpen: boolean;
@@ -6,12 +11,63 @@ interface NewTransactionModalProps {
 }
 
 export function NewTransactionModal({isOpen , onRequestClose}: NewTransactionModalProps) {
+    const [type, setType] = useState('deposit');
+
     return(
         <Modal 
             isOpen={isOpen} 
             onRequestClose={onRequestClose}
+            overlayClassName="react-modal-overlay"
+            className="react-modal-content"
         >
-        <h2>Modalzada</h2>
+            <button 
+                type="button" 
+                onClick={onRequestClose} 
+                className="react-modal-close"
+            >
+                <img src={closeImg} alt="Fechar"/>
+            </button>
+            <Container>
+                <h2>Cadastrar transação</h2>
+                <input type="text"
+                    placeholder="Título"
+                />
+
+                <input
+                    type="number"
+                    placeholder="Valor"
+                />
+
+                <TransctionTypeContainer>
+                    <TypeBox 
+                        type="button"
+                        onClick={() => setType('deposit')}
+                        isActive={type === 'deposit'}
+                        activeColor="green"
+                    >
+                        <img src={incomeImg} alt="Entrada"/>
+                        <span>Entrada</span>
+                    </TypeBox>
+
+                    <TypeBox 
+                        type="button"
+                        onClick={() =>  {setType('withdraw'); }}
+                        isActive={type === 'withdraw'}
+                        activeColor="red"
+                    >
+                        <img src={outcomeImg} alt="Saída"/>
+                        <span>Saída</span>
+                    </TypeBox>
+                </TransctionTypeContainer>
+
+                <input
+                    placeholder="Categoria"
+                />
+
+                <button type="submit">
+                    Cadastrar
+                </button>
+            </Container>
         </Modal>
     )
 }
